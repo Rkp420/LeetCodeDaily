48e4a3d5d6ea5255f905a23442f5c13abd6a64f3
Rkp420 <rkp20442@gmail.com>
2025-04-12 17:33:29 +0530

    12-Apr-2025 POTD Solved

---
+++ Apr-2025/Day-12.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+// https://leetcode.com/problems/find-the-count-of-good-integers/?envType=daily-question&envId=2025-04-12
+
+class Solution
+{
+public:
+    long long countGoodIntegers(int n, int k)
+    {
+
+        unordered_set<string> st;
+
+        int d = (n + 1) / 2;
+        int start = pow(10, d - 1);
+        int end = pow(10, d) - 1;
+
+        for (int num = start; num <= end; num++)
+        {
+            string leftHalf = to_string(num);
+
+            string rightHalf = "";
+            if (n % 2 == 0)
+            {
+                rightHalf = leftHalf;
+                reverse(rightHalf.begin(), rightHalf.end());
+            }
+            else
+            {
+                rightHalf = leftHalf.substr(0, d - 1);
+                ;
+                reverse(rightHalf.begin(), rightHalf.end());
+            }
+
+            string full = leftHalf + rightHalf;
+            long long number = stoll(full);
+            if (number % k != 0)
+                continue;
+
+            sort(full.begin(), full.end());
+            st.insert(full);
+        }
+
+        vector<long long> factorial(11, 1);
+        for (int i = 1; i < 11; i++)
+        {
+            factorial[i] = (factorial[i - 1] * i);
+        }
+
+        long long res = 0;
+        for (const string &s : st)
+        {
+            vector<int> count(10, 0);
+            for (const char &ch : s)
+                count[ch - '0']++;
+
+            int totalDigits = s.length();
+            int zeroDigits = count[0];
+            int nonZeroDigits = totalDigits - zeroDigits;
+
+            long long perm = (nonZeroDigits * factorial[totalDigits - 1]);
+
+            for (int i = 0; i < 10; i++)
+            {
+                perm /= factorial[count[i]];
+            }
+
+            res += perm;
+        }
+
+        return res;
+    }
+};
+
+int main()
+{
+    return 0;
+}
