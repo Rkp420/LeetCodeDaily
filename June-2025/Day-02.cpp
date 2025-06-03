f8b8995231161559ee18060982e301d268d0d59c
Rkp420 <rkp20442@gmail.com>
2025-06-03 09:10:17 +0530

    Till 03-June-2025 Potd Solved

---
+++ June-2025/Day-02.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// https://leetcode.com/problems/candy/?envType=daily-question&envId=2025-06-02
+
+class Solution
+{
+public:
+    int candy(vector<int> &ratings)
+    {
+        int n = ratings.size();
+        if (n == 1)
+            return 1;
+        vector<int> left(n, 1);
+        for (int i = 1; i < n; i++)
+        {
+            if (ratings[i] > ratings[i - 1])
+            {
+                left[i] = left[i - 1] + 1;
+            }
+        }
+
+        int right = 1;
+        int sum = left[n - 1];
+        for (int j = n - 2; j >= 0; j--)
+        {
+            if (ratings[j] > ratings[j + 1])
+            {
+                right++;
+                left[j] = max(left[j], right);
+            }
+            else
+            {
+                right = 1;
+            }
+            sum += left[j];
+        }
+
+        return sum;
+    }
+};
+
+int main()
+{
+    return 0;
+}
