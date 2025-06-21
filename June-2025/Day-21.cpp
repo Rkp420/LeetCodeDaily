ceca998170e865ec794dcacb4e4bd1152285805e
Rkp420 <rkp20442@gmail.com>
2025-06-21 16:39:43 +0530

    21-JUNE-2025 POTD Solved

---
+++ June-2025/Day-21.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// https://leetcode.com/problems/minimum-deletions-to-make-string-k-special/description/?envType=daily-question&envId=2025-06-21
+
+class Solution
+{
+public:
+    int minimumDeletions(string word, int k)
+    {
+        unordered_map<char, int> cnt;
+        for (auto &ch : word)
+        {
+            cnt[ch]++;
+        }
+        int res = word.size();
+        for (auto &[_, a] : cnt)
+        {
+            int deleted = 0;
+            for (auto &[_, b] : cnt)
+            {
+                if (a > b)
+                {
+                    deleted += b;
+                }
+                else if (b > a + k)
+                {
+                    deleted += b - (a + k);
+                }
+            }
+            res = min(res, deleted);
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    return 0;
+}
