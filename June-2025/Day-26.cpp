7af0b7485de00150aca030eea2552bf584a13d6d
Rkp420 <rkp20442@gmail.com>
2025-06-26 08:40:53 +0530

    26-JUNE-2025 POTD Solved

---
+++ June-2025/Day-26.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int longestSubsequence(string s, int k)
+    {
+        int oneCount = 0;
+        int num = 0;
+        int pow = 1;
+
+        // Take as many 1s as possible from the right.
+        for (int i = s.length() - 1; i >= 0 && num + pow <= k; --i)
+        {
+            if (s[i] == '1')
+            {
+                ++oneCount;
+                num += pow;
+            }
+            pow *= 2;
+        }
+
+        return ranges::count(s, '0') + oneCount;
+    }
+};
+
+int main()
+{
+    return 0;
+}
