706ba335bd1bffeb75ad1001fc988afe1fd58360
Rkp420 <rkp20442@gmail.com>
2025-07-07 22:30:32 +0530

    07-July-2025 POTD Solved

---
+++ July-2025/Day-07.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+// https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/description/?envType=daily-question&envId=2025-07-07
+
+class Solution
+{
+public:
+    int maxEvents(vector<vector<int>> &events)
+    {
+        int n = events.size();
+        int maxDay = 0;
+        for (int i = 0; i < events.size(); i++)
+        {
+            maxDay = max(maxDay, events[i][1]);
+        }
+        priority_queue<int, vector<int>, greater<>> pq;
+        sort(events.begin(), events.end());
+        int ans = 0;
+        for (int i = 0, j = 0; i <= maxDay; i++)
+        {
+            while (j < n && events[j][0] <= i)
+            {
+                pq.emplace(events[j][1]);
+                j++;
+            }
+            while (!pq.empty() && pq.top() < i)
+            {
+                pq.pop();
+            }
+            if (!pq.empty())
+            {
+                pq.pop();
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
